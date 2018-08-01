977276e277d6f86263ad6b69e1dd8cc6b6436a7a
pietre.linux <pietre.linux@gmail.com>
2018-08-01 17:03:13 +0200

    Update TableX.sh

--- TableX.sh
+++ TableX.sh
@@ -21,7 +21,7 @@ mkdir /home/sunxi/kernel/modules
 mkdir /home/sunxi/kernel/mainline
 mkdir /home/sunxi/kernel/zimage
 mkdir /mnt/ramdisk
-mount -t tmpfs -o size=10240m tmpfs /mnt/ramdisk
+mount -t tmpfs -o size=1024M tmpfs /mnt/ramdisk
 clear
 echo " Directorios creados "
 cp TableX_defconfig /home/sunxi
